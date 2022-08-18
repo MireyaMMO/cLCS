cd62c0b545fa23a9f278fdfe97c55d091f26d51d
MireyaMMO <61437663+MireyaMMO@users.noreply.github.com>
2022-08-18 13:17:45 +1200

    Create __init__.py

---
+++ cLCS/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Moana Otago PORG tools
+"""
+
+__authors__ = ['']
+__copyright__ = "left"
+__credits__ = [""]
+__license__ = "Private"
+__maintainer__ = ""
+__email__ = "" 
+__status__ = "Beta"
+
+__all__ = ['mean_C.py', 'cLCS.py', 'plotting.py']
