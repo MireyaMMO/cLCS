5b042105ea55145be5a1881882c2c8bf7717f701
MireyaMMMO <mireya.mmor@gmail.com>
2023-12-25 18:49:28 +1300

    Finalising first complete release, formatting

--- cLCS/__init__.py
+++ cLCS/__init__.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""
-Moana Otago PORG tools
-"""
+__authors__ = ['Mireya Montano']
+__credits__ = ["Rodrigo Duran"]
+__license__ = ""
+__maintainer__ = "Mireya Montano"
+__email__ = "mireya.mmor@gmail.com" 
+__status__ = "Release"
 
-__authors__ = ['']
-__copyright__ = "left"
-__credits__ = [""]
-__license__ = "Private"
-__maintainer__ = ""
-__email__ = "" 
-__status__ = "Beta"
-
-__all__ = ['mean_C.py', 'cLCS.py', 'plotting.py']
+__all__ = ['mean_C.py', 'make_cLCS.py', 'plotting_cLCS.py', 'utils.py']
